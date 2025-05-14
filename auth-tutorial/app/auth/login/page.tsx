8a1d67ec50d9d58f4e2cfaef79efcbc70d8bcff9
yasterlij <108715240+yasterlij@users.noreply.github.com>
2025-05-14 06:32:14 +0000

    auth and login page added

---
+++ auth-tutorial/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+    return (
+        <div>
+        <h1>Login Page</h1>
+        <p>Login to your account</p>
+        {/* Add your login form here */}
+        </div>
+    );
+    }
+export default LoginPage;
