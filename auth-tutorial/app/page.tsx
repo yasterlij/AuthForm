2cb898cebba11dbad031c6363d5ff39817536692
yasterlij <108715240+yasterlij@users.noreply.github.com>
2025-05-14 07:52:24 +0000

    page.tsx added

---
+++ auth-tutorial/app/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+
+        </main>
+    )
+}
