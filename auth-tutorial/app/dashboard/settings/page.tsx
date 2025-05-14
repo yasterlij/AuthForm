9861c847331356a28e8c53a68b473258d3dd2376
yasterlij <108715240+yasterlij@users.noreply.github.com>
2025-05-14 07:45:00 +0000

    Login page added

--- auth-tutorial/app/dashboard/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-const SettingsPage = () => {
-    return (
-        <div>
-        <h1>Settings</h1>
-        <p>Settings page content goes here.</p>
-        </div>
-    );
-    }
-export default SettingsPage;
--- auth-tutorial/app/dashboard/users/page.tsx
+++
@@ -1,9 +0,0 @@
-const UsersPage = () => {
-    return (
-        <div>
-            Users Page!
-        </div>
-    )
-}
-export default UsersPage;
-// This is the page that will be displayed when the user navigates to /dashboard/users
